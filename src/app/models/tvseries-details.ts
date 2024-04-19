1cce49337ceb582bd25217b5d7d248dd8c164b97
Timothy jan <jan.timothy@gmail.com>
2024-04-18 17:58:55 -0700

    Added TV Series details model

---
+++ src/app/models/tvseries-details.ts
@@ -0,0 +1,17 @@
+export interface TvSeriesDetails {
+  air_date: string;
+  crew: string[];
+  episode_number: number;
+  guest_stars: string[];
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  still_path: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+
